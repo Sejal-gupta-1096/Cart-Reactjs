3f3a01412ebcc327caae2a902846a71b1e9d4d04
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-06-28 01:27:22 -0700

    increase and decrease quantity by changing state and re rendering it

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,48 @@ import React from 'react';
 import './index.css'
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price : 999,
+            title : 'Phone',
+            qty : 2,
+            img : ""
+        }
+    }
+    increaseQuantity = () => {
+        console.log('this' , this);
+        this.setState( (prevState) => {
+            return {
+                qty : prevState.qty + 1
+            }
+        });
+    }
+
+    decreaseQuantity = () => {
+        console.log('this' , this);
+        this.setState( (prevState) => {
+            return {
+                qty : prevState.qty - 1
+            }
+        });
+    }
+
     render(){
+        const {title , qty , price} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize : 25}}>Phone</div>
-                    <div style={{color : '#777'}}>Rs. 999</div>
-                    <div style={{color : '#777'}}>Qty: 2</div>
+                    <div style={{fontSize : 25}}>{title}</div>
+                    <div style={{color : '#777'}}>Rs. {price}</div>
+                    <div style={{color : '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        {/*todo*/}
+                        <img onClick={this.increaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg" />
+                        <img onClick={this.decreaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/svg/929/929430.svg" />
+                        <img className="action-icons" src="https://image.flaticon.com/icons/svg/3089/3089818.svg" />
                     </div>
                 </div>
             </div>
