2a1daefb36d9d587ade7233c5cca3fc6f09e9fb9
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-06-29 04:33:16 -0700

    Implementing everything via props

--- src/Cart.js
+++ src/Cart.js
@@ -4,12 +4,73 @@ import './index.css'
 
 class Cart extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            products : [
+                {
+                    price : 10000,
+                    title : 'Phone',
+                    qty : 20,
+                    img : "",
+                    id : 1
+                } , 
+                {
+                    price : 2000,
+                    title : 'Wath',
+                    qty : 50,
+                    img : "",
+                    id : 2
+                } , 
+                {
+                    price : 60000,
+                    title : 'Laptop',
+                    qty : 10,
+                    img : "",
+                    id : 3
+                }
+            ]
+        }
+    }
+
+    handleIncreaseQuantity = (product) => {
+        let {products} = this.state;
+        let index = products.indexOf(product);
+
+        products[index].qty = product.qty + 1;
+
+        this.setState({
+            products : products
+        });
+    }
+
+    handleDecreaseQuantity = (product) => {
+        let {products} = this.state;
+        let index = products.indexOf(product);
+
+        if(products[index].qty > 0){
+            products[index].qty = product.qty - 1;
+        }
+        
+        this.setState({
+            products : products
+        });
+    }
+
+
     render(){
+        const {products} = this.state
         return(
             <div className ='cart'>
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map( (product) => {
+                    return <CartItem 
+                        product={product} 
+                        key={product.id} 
+                        handleIncreaseQuantity={this.handleIncreaseQuantity} 
+                        handleDecreaseQuantity={this.handleDecreaseQuantity} 
+                    />
+                })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,40 +2,10 @@ import React from 'react';
 import './index.css'
 
 class CartItem extends React.Component{
+
     
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            title : 'Phone',
-            qty : 2,
-            img : ""
-        }
-    }
-
-    increaseQuantity = () => {
-        console.log('this' , this);
-        this.setState( (prevState) => {
-            return {
-                qty : prevState.qty + 1
-            }
-        });
-    }
-
-    decreaseQuantity = () => {
-        console.log('this' , this);
-        this.setState( (prevState) => {
-            if(prevState.qty == 0){
-                return;
-            }
-            return {
-                qty : prevState.qty - 1
-            }
-        });
-    }
-
     render(){
-        const {title , qty , price} = this.state;
+        const {title , qty , price} = this.props.product;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -46,8 +16,8 @@ class CartItem extends React.Component{
                     <div style={{color : '#777'}}>Rs. {price}</div>
                     <div style={{color : '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        <img onClick={this.increaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg" />
-                        <img onClick={this.decreaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/svg/929/929430.svg" />
+                        <img onClick={() => this.props.handleIncreaseQuantity(this.props.product)} className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg" />
+                        <img onClick={() => this.props.handleDecreaseQuantity(this.props.product)} className="action-icons" src="https://image.flaticon.com/icons/svg/929/929430.svg" />
                         <img className="action-icons" src="https://image.flaticon.com/icons/svg/3089/3089818.svg" />
                     </div>
                 </div>
