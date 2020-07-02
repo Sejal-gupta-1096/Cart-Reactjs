4f91c42cc13325c2e19cbc681d818fc228a2c482
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-07-02 00:34:59 -0700

    Add products to firebase and render them

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
 import Cart from './Cart'
 import Navbar from './Navbar'
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-        products : [
-            {
-                price : 10000,
-                title : 'Phone',
-                qty : 20,
-                img : "https://images.pexels.com/photos/265658/pexels-photo-265658.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                id : 1
-            } , 
-            {
-                price : 2000,
-                title : 'Wath',
-                qty : 50,
-                img : "https://images.pexels.com/photos/277390/pexels-photo-277390.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                id : 2
-            } , 
-            {
-                price : 60000,
-                title : 'Laptop',
-                qty : 10,
-                img : "https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                id : 3
-            }
-        ]
+        products : [],
+        loading : true
     }
 }
 
@@ -90,8 +71,28 @@ class App extends React.Component {
     return total;
   }
 
+  componentDidMount = () => {
+
+    firebase
+    .firestore()
+    .collection('products')
+    .onSnapshot( (snapshot) => {
+      console.log(snapshot);
+      const products = snapshot.docs.map( (doc) => {
+        let data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      })
+
+      this.setState({
+        products : products,
+        loading : false
+      })
+    }) 
+  }
+
   render(){
-    const {products} = this.state
+    const {products , loading} = this.state
     return (
       <div className="App">
         <Navbar count={this.getCount()} />
@@ -102,6 +103,7 @@ class App extends React.Component {
           handleDeleteProduct={this.handleDeleteProduct} 
         />
         <div style={{fontSize:"25px" , padding:"20px"}}>TOTAL PRICE : {this.getTotal()}</div>
+        {loading && <h1>Loading Products...</h1>}
       </div>
     );
   }
